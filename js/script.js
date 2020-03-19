c13c3988a4818c17fbd15636e3bdff78f174c701
ucef8233 <-u>
2020-03-19 13:56:37 +0100

    Ko

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-let test = document.querySelector("main").children;
+let cas = document.querySelector("main").children;
+let resultat = document.querySelector(".res").children;
 let j = 1;
+let colorTab = [];
+var resultX = 0;
+var resultO = 0;
 var tabResult = [
   [0, 1, 2],
   [0, 3, 6],
@@ -10,19 +14,20 @@ var tabResult = [
   [1, 4, 7],
   [3, 4, 5]
 ];
-for (let i = 0; i < test.length; i++) {
+
+for (let i = 0; i < cas.length; i++) {
   var tabVerif = [];
   var tabX = [];
   var tabO = [];
-  test[i].addEventListener("click", function() {
+  cas[i].addEventListener("click", function() {
     j++;
     if (j % 2 == 0) {
       if (verif(i)) {
         alert("case plaine");
         j--;
       } else {
-        test[i].innerHTML = "X";
-        test[i].classList.add("red");
+        cas[i].innerHTML = "X";
+        cas[i].classList.add("black");
         tabVerif.push(i);
         tabX.push(i);
       }
@@ -31,8 +36,8 @@ for (let i = 0; i < test.length; i++) {
         alert("case plaine");
         j--;
       } else {
-        test[i].innerHTML = "O";
-        test[i].classList.add("blue");
+        cas[i].innerHTML = "O";
+        cas[i].classList.add("black");
         tabVerif.push(i);
         tabO.push(i);
       }
@@ -41,7 +46,7 @@ for (let i = 0; i < test.length; i++) {
   });
 }
 var verif = function(i) {
-  for (let j = 0; j < test.length; j++) {
+  for (let j = 0; j < cas.length; j++) {
     if (tabVerif[j] == i) {
       return true;
     }
@@ -60,7 +65,15 @@ var result = function(a, b) {
         }
       }
       if (test == 3) {
-        alert("win X");
+        resultX = resultX + 1;
+        resultat[0].innerHTML = resultX;
+        resultat[1].innerHTML = resultO;
+        colorTab = tabResult[i];
+        resultColor(cas, colorTab);
+        replay();
+      }
+      if (tabVerif.length == 9) {
+        replay();
       }
     }
   } else if (j % 2 == 1) {
@@ -73,10 +86,41 @@ var result = function(a, b) {
           }
         }
       }
-      if (test == 3) {
-        alert("win O");
 
+      if (test == 3) {
+        resultO = resultO + 1;
+        resultat[1].innerHTML = resultO;
+        resultat[0].innerHTML = resultX;
+        colorTab = tabResult[i];
+        resultColor(cas, colorTab);
+        replay();
+      }
+      if (tabVerif.length == 9) {
+        replay();
       }
     }
   }
+};
+var resultColor = function(x, y) {
+  for (let e = 0; e < x.length; e++) {
+    for (let r = 0; r < y.length; r++) {
+      if (y[r] == [e]) {
+        x[e].classList.add("bg");
+        window.setTimeout(function() {
+          x[e].classList.remove("bg");
+        }, 1500);
+      }
+    }
+  }
+};
+var replay = function() {
+  for (let j = 0; j < 9; j++) {
+    cas[j].innerHTML = "";
+  }
+
+  tabX = [];
+  tabO = [];
+  tabVerif = [];
+  test = 0;
+  colorTab = [];
 };
